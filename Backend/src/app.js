a14aec673e2b2a423e9abc36910acceb7f978a6d
Santiago Cisneros <santiagocisneros046@gmail.com>
2026-04-03 16:08:00 -0500

    Implementación de proyectos para conexión con el Frontend

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import productRoutes from "./routes/product.routes.js";
 import inventoryRoutes from "./routes/inventory.routes.js";
 import salesRoutes from "./routes/sales.routes.js";
@@ -8,6 +9,7 @@ import { errorHandler } from "./middlewares/error.middleware.js";
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/products", productRoutes);
